f79152cdc28f17c6d84121ea5d93248609a14e44
Jo Matsumaru <hatunehimeka@outlook.jp>
2024-11-01 20:46:47 +0900

    Initial commit

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Velopack;
+using Velopack.Sources;
+
+namespace WpfApp1;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    
+    
+    private static async Task UpdateCheck()
+    {
+        try
+        {
+            /*httpsやgitlabなども設定できます。
+今回は、githubで設定しました。ダウングレードやStable、Betaのチャンネル切り替えも出来ますが、今回はシンプルな構造にしました。
+                                        */
+            var mgr = new UpdateManager(new GithubSource(@"https://github.com/<Your Name>/<Your Proj>", null, false),
+                new UpdateOptions
+                {
+                    AllowVersionDowngrade = true
+                });
+
+            if (!mgr.IsInstalled)
+            {
+                Console.WriteLine("Application is not installed. Update check cannot proceed.");
+                return;
+            }
+
+            var newVersionCheck = await mgr.CheckForUpdatesAsync();
+            if (newVersionCheck == null) return;
+
+            var result = MessageBox.Show("New Version Available", "Update", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                await mgr.DownloadUpdatesAsync(newVersionCheck);
+                mgr.ApplyUpdatesAndRestart(newVersionCheck);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private async void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        await UpdateCheck();
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+    }
+}
---
+++ WpfApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using Velopack;
+
+namespace WpfApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            //velopackの初期化&起動 .WithFirstRunで初めて起動したときの制御
+            VelopackApp.Build().WithBeforeUninstallFastCallback((v) => { }).WithFirstRun((v) =>
+            {
+                MessageBox.Show("Thanks for installing my application!");
+            }).Run();
+                
+            //velopackの初期化と起動そしてアップデートチェック時にもアプリケーションを動作させたいので、別スレッドで起動
+            Thread thread = new Thread(() =>
+            {
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+}
